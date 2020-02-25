65b9e041780da8240f94408e720ea02bc9346e23
Ricardo199 <ricardo1529@gmail.com>
2020-02-25 11:03:45 -0600

    commit

---
+++ views/comisiones.php
@@ -0,0 +1,16 @@
+<form action="comisiones.php" method="POST">
+    Introdusca su salario por favor <input type="text" name="sal" required/>
+    <input type="submit" value="enviar" name="enviar"/>
+</form>
+<?php 
+    if(isset($_REQUEST['enviar'])){
+        $sal=$_REQUEST['sal'];
+        $comi=$sal*0.10+1;
+        $tot3=$sal+($comi*3);
+        $tot10=$sal+($comi*10);
+        $tot15=$sal+($comi*15);
+        echo "Su salario por 3 ventas es: $".$tot3."<br>";
+        echo "Su salario por 10 ventas es: $".$tot10."<br>";
+        echo "Su salario por 15 ventas es: $".$tot15."<br>";
+    }
+?>
---
+++ views/ganancia.php
@@ -0,0 +1,15 @@
+<form action="ganancia.php" method="POST">
+    Introdusca el monto inical por favor <input type="text" name="mon" required/>
+    <input type="submit" value="enviar" name="enviar"/>
+</form>
+<?php 
+    if(isset($_REQUEST['enviar'])){
+        $mon=$_REQUEST['mon'];
+        $int=$mon*0.035;
+        echo "Intereces a los tres meces: $".$int*3;
+        echo "<br>";
+        echo "Intereces a los seis meces: $".$int*6;
+        echo "<br>";
+        echo "Intereces a los doce meces: $".$int*12;
+    }
+?>
